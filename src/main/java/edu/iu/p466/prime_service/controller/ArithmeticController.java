fcd6886b519b6f512cdf402792cc23af2a866f5a
alexllapointe <alexanderllapointe@gmail.com>
2025-03-30 16:32:39 -0400

    update

---
+++ src/main/java/edu/iu/p466/prime_service/controller/ArithmeticController.java
@@ -0,0 +1,29 @@
+package edu.iu.p466.prime_service.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.iu.p466.prime_service.service.IArithmeticService;
+
+@RestController
+@CrossOrigin
+public class ArithmeticController {
+
+    IArithmeticService arithmeticService;
+
+    public ArithmeticController(IArithmeticService arithmeticService){
+        this.arithmeticService = arithmeticService;
+    }
+
+    @GetMapping("/")
+    public String greetings(){
+        return "Welcome to the Arithmetic service!";
+    }
+
+    @GetMapping("/{n}/{m}")
+    public int add(@PathVariable int n, @PathVariable int m){
+        return arithmeticService.add(n, m);
+    }
+}
---
+++ src/main/java/edu/iu/p466/prime_service/service/ArithmeticService.java
@@ -0,0 +1,9 @@
+package edu.iu.p466.prime_service.service;
+
+public class ArithmeticService implements IArithmeticService {
+
+    @Override
+    public int add(int n, int m) {
+        return n + m;
+    }
+}
